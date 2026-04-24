0e40e794733191beaad0bf35c22f0f978859fd3f
Ricardo Cardenes <ricardo.cardenes@noirlab.edu>
2026-04-24 11:35:07 -0400

    Use Themery for color scheme selection

--- plugin/00-theme.lua
+++ plugin/00-theme.lua
@@ -2,7 +2,21 @@ vim.pack.add({
     { src = 'https://github.com/rose-pine/neovim', name = 'rose-pine' },
     { src = 'https://github.com/ful1e5/onedark.nvim', name = 'onedark' },
     { src = 'https://github.com/EdenEast/nightfox.nvim', name = 'nightfox' },
+    { src = 'https://github.com/rebelot/kanagawa.nvim', name = 'kanagawa' },
+
+    'https://github.com/zaldih/themery.nvim',
 })
 
-require("rose-pine").setup()
-vim.cmd.colorscheme("rose-pine")
+local available_themes = {
+    "rose-pine",
+    "onedark",
+    "nightfox",
+    { name = "Kanagawa (Wave)", colorscheme = "kanagawa-wave"},
+    { name = "Kanagawa (Lotus)", colorscheme = "kanagawa-lotus" },
+    { name = "Kanagawa (Dragon)", colorscheme = "kanagawa-dragon" },
+}
+
+require("themery").setup({
+    themes = available_themes,
+    livePreview = true,
+})
