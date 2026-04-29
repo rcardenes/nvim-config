0225b0edd652907a7fd8e5a1ca29e252da57b71c
Ricardo Cárdenes <ricardo.cardenes@gmail.com>
2026-04-29 10:02:14 -0400

    Change tab settings for C/C++ files

---
+++ ftplugin/cpp.lua
@@ -0,0 +1,5 @@
+-- Only apply to C++ files
+vim.bo.expandtab = false -- Use hard tabs
+vim.bo.tabstop = 4       -- 4 spaces per tab
+vim.bo.shiftwidth = 4    -- 4 spaces for auto-indent
+vim.bo.softtabstop = 0   -- Use 0 to make softtabstop follow tabstop
